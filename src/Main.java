157bb6727e170b419751daf4f52f493806ac08ca
abdullah <110098187+acoplu@users.noreply.github.com>
2023-01-23 16:43:10 +0300

    "ROW" and "COLUMN" prompts for input.

--- src/Main.java
+++ src/Main.java
@@ -10,25 +10,29 @@ public class Main {
 
             Memory.printBoard();
 
-            System.out.println("Please enter the 2 index you want to flip:");
+            System.out.println("Please enter the first indexes of first card you want to flip: ");
+            System.out.print("[ROW-1] --> ");
             index1=scan.nextInt();
+            System.out.print("[COLUMN-1] --> ");
             index2=scan.nextInt();
-            check=Memory.flipFirstCard(index1, index2); // return false if we made illegal move
+            check=Memory.flipFirstCard(index1-1, index2-1); // return false if we made illegal move
             if(check==false) {
                 System.out.println("Illegal move...\n\n\n");
                 continue;
             }
 
-            System.out.println("Please enter the 2 index you want to flip:");
+            System.out.println("Please enter the indexes of second card you want to flip: ");
+            System.out.print("[ROW-2] --> ");
             index3=scan.nextInt();
+            System.out.print("[COLUMN-2] --> ");
             index4=scan.nextInt();
-            check=Memory.flipSecondCard(index1, index2, index3, index4);
+            check=Memory.flipSecondCard(index1-1, index2-1, index3-1, index4-1);
             if(check==false){
                 System.out.println("Illegal move...\n\n\n");
                 continue;
             }
 
-            Memory.matchCards(index1, index2, index3, index4);
+            Memory.matchCards(index1-1, index2-1, index3-1, index4-1);
             System.out.println("\n\n\n");
         }
 
